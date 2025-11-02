a7a22967b56b09cde42e59aba1d5cc4a137ed470
Thomas Debay <thomas@debay.org>
2025-11-02 11:27:02 +0100

    Fix toggle switch

--- resources/views/vendor/backpack/crud/buttons/toggle.blade.php
+++ resources/views/vendor/backpack/crud/buttons/toggle.blade.php
@@ -1,12 +1,12 @@
 @if($entry->trashed())
-<form action="{{ url()->previous()}}/{{ $entry->getKey() }}/restore" method="post">
+<form action="{{ url()->previousPath()}}/{{ $entry->getKey() }}/restore" method="post">
 	@csrf
 	<button type="submit" class="btn btn-sm btn-success">
 		<i class="la la-check"></i> @lang('Enable')
 	</button>
 </form>
 @else
-<form action="{{ url()->previous()}}/{{ $entry->getKey() }}/delete" method="post">
+<form action="{{ url()->previousPath()}}/{{ $entry->getKey() }}/delete" method="post">
 	@csrf
 	<button type="submit" class="btn btn-sm btn-danger">
 		<i class="la la-trash"></i> @lang('Disable')
