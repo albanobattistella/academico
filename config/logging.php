ad8905843287ad893ec1181ad78dd34fa9318044
Thomas Debay <thomas@debay.org>
2026-03-14 08:22:05 +0100

    build docker image in github CI

--- config/logging.php
+++ config/logging.php
@@ -67,7 +67,7 @@ return [
 
         'daily' => [
             'driver' => 'daily',
-            'path' => storage_path('logs/laravel.log'),
+            'path' => storage_path(env('LOG_DAILY_PATH', 'logs/laravel.log')),
             'level' => env('LOG_LEVEL', 'debug'),
             'days' => env('LOG_DAILY_DAYS', 14),
             'replace_placeholders' => true,
